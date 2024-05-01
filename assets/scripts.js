862abd66bee5bddbbe9f788d4aaa2f44b847ea6d
Fabien Gosse <f.gosse77@gmail.com>
2024-05-01 21:47:58 +0200

    add optimized img

--- assets/scripts.js
+++ assets/scripts.js
@@ -1,15 +1,33 @@
-$(document).ready(function() {
-    $('.gallery').mauGallery({
-        columns: {
-            xs: 1,
-            sm: 2,
-            md: 3,
-            lg: 3,
-            xl: 3
-        },
-        lightBox: true,
-        lightboxId: 'myAwesomeLightbox',
-        showTags: true,
-        tagsPosition: 'top'
-    });
+/*document.addEventListener("DOMContentLoaded", function () {
+  let gallery = document.querySelector(".gallery");
+
+  gallery = {
+    columns: {
+      xs: 1,
+      sm: 2,
+      md: 3,
+      lg: 3,
+      xl: 3,
+    },
+    lightBox: true,
+    lightboxId: "myAwesomeLightbox",
+    showTags: true,
+    tagsPosition: "top",
+  };
+});*/
+
+$(document).ready(function () {
+  $(".gallery").mauGallery({
+    columns: {
+      xs: 1,
+      sm: 2,
+      md: 3,
+      lg: 3,
+      xl: 3,
+    },
+    lightBox: true,
+    lightboxId: "myAwesomeLightbox",
+    showTags: true,
+    tagsPosition: "top",
+  });
 });
